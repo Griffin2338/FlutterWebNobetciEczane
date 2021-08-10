ee8b89365af09660ea63a4ba2c47295f5754055d
Griffin2338 <72986426+Griffin2338@users.noreply.github.com>
2021-08-10 11:00:03 +0300

    dsacsa

--- flutter_webdeneme/android/app/src/main/kotlin/com/example/flutter_webdeneme/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_webdeneme
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/data/Eczane_api.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-import 'package:http/http.dart' as http;
-
-class EczaneApi {
-  static Future getCharacters() {
-    return http.get(Uri.parse("https://www.netdata.com/JSON/a40cee7b"));
-
-  }
-}
